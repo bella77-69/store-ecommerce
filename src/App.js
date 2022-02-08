86cf822fdee4745b5b99f25c7ee64c2be7831284
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-07 18:31:58 -0800

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.png';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.png";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-  <div className='container'>
-    <div className='row'>
-      <div className='col-6'>column number one</div>
-      <div className='col-6'><span><i className='fas fa-home'></i></span></div>
-    </div>
-  </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
