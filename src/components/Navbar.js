b80aeb754a1c11d4b1808d97b46f4145b2d6eb84
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-07 18:52:11 -0800

    navbar component completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.png";
+import "../Styles/scss/_images.scss";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          {" "}
+          <img src={logo} alt="app-logo" className="navbar-brand navbar-logo" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus">my cart</i>
+          </button>
+        </Link>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime modfasf dsafdasdfsa sdfasf sdfasdf et wsdgsag gsg ahsdgsadg ashaghsdgcbdfber rtdgdg",
+  },
+];
