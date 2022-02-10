3010510e12c0c73d8b056fa97eb8f433c3ed5f12
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-09 20:40:49 -0800

    default component finished

--- src/context.js
+++ src/context.js
@@ -100,11 +100,10 @@ class ProductProvider extends Component {
     const index = tempCart.indexOf(selectedProduct);
     const product = tempCart[index];
 
-    product.count = product.count -1;
-    if(product.count === 0) {
+    product.count = product.count - 1;
+    if (product.count === 0) {
       this.removeItem(id);
-    }
-    else{
+    } else {
       product.total = product.count * product.price;
       this.setState(
         () => {
